9a49475b61d9d3fb3cded5f0b8aac1e0a537599c
santosdlaysa <santosdlaysa@gmail.com>
2023-12-23 11:16:07 -0400

    at

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,16 @@
 import Styles from '../index.module.css';
+import React from 'react';
+
+
 function Nav(){
     return(
         <>
              <nav>
-        <a href="#home" className={Styles.navButtons}>Home</a>
-        <a href="#tecnologias" className={Styles.navButtons}>Tecnologias</a>
-        <a href="#sobre" className={Styles.navButtons}>Sobre mim</a>
-        <a href="#contatos" className={Styles.navButtons}>Contatos</a>
-        <a href="#projetos" className={Styles.buttonProjeto}>Projetos</a>
+        <a href="santosdlaysa.github.io/Portfolio-oficial" className={Styles.navButtons}>Home</a>
+        <a href="./Tecnologias" className={Styles.navButtons}>Tecnologias</a>
+        <a href="#Sobre" className={Styles.navButtons}>Sobre mim</a>
+        <a href="#Contatos" className={Styles.navButtons}>Contatos</a>
+        <a href='#projetos' className={Styles.buttonProjeto}>Projetos/</a>
       </nav>
       <div class="nav__btns">
         <i class="uil uil-moon change-theme" id="theme-button"></i>
