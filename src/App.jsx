68ea86d7a12021bd9a47eb8c7585ff0cc7734459
santosdlaysa <santosdlaysa@gmail.com>
2023-12-26 15:58:51 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Nav from "./components/Nav";
-import logo from "./icons/logo.png"; // Importe a imagem
-import Styles from '../src/index.module.css';
 import Perfil from "./components/Perfil";
 import Sobre from "./components/Sobre";
 import Projetos from "./components/Projetos";
@@ -11,13 +9,7 @@ import Contatos from "./components/Contatos";
 function App() {
   return (
     <>
-      <header className={Styles.mainHeader}>
-        <img className={Styles.logo2} src={logo} alt="Logo" /> {/* Use o atributo src com o caminho da imagem */}
-
-        <nav className={Styles.nav}>
-          <Nav />
-        </nav>
-      </header>
+      <Nav />
       <Perfil />
       <Sobre />
       <Projetos />
